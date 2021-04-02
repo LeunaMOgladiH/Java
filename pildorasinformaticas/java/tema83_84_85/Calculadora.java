d5e46e97e2f80f606b50fe8450a05369db2237b7
Manuel Hidalgo Delfin <manuelhidalgodelfin@gmail.com>
2021-04-02 17:19:53 +0200

    Trabajando con Textos

--- pildorasinformaticas/java/tema83_84_85/Calculadora.java
+++ pildorasinformaticas/java/tema83_84_85/Calculadora.java
@@ -1,4 +1,4 @@
-package tema83;
+package tema83_84_85;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
@@ -59,38 +59,41 @@ class LaminaCalc extends JPanel{
 		
 		ActionListener insertar= new Insertanum(); // creamos una instancia de la clase insertanum
 		
-		//ponerBoton("%");
-		//ponerBoton("CE");
-		//ponerBoton("C");
-		//ponerBoton("DEL");
+		ActionListener orden=new AccionOrden(); // creamos una instancia que este a la escucha de las operaciones numericas 
 		
-		//ponerBoton("1/x");
-		//ponerBoton("x^2");
-		//ponerBoton("x^1/2");
-		//ponerBoton("/");
+		ponerBoton("%",orden);
+		ponerBoton("CE",orden);
+		ponerBoton("C",orden);
+		ponerBoton("DEL",orden);
+		
+		ponerBoton("1/x",orden);
+		ponerBoton("x^2",orden);
+		ponerBoton("x^1/2",orden);
+		ponerBoton("/",orden);
 		
 		ponerBoton("7",insertar);
 		ponerBoton("8",insertar);
 		ponerBoton("9",insertar);
-		//ponerBoton("x",insertar);
+		ponerBoton("x",orden);
 		
 		ponerBoton("4",insertar);
+		ponerBoton("5",insertar);
 		ponerBoton("6",insertar);
-		ponerBoton("7",insertar);
-	  //ponerBoton("-");
+		ponerBoton("-",orden);
 		
 		ponerBoton("1",insertar);
 		ponerBoton("2",insertar);
 		ponerBoton("3",insertar);
-		//ponerBoton("+");
+		ponerBoton("+",orden);
 		
-		//ponerBoton("+/-");
+		ponerBoton("+/-",orden);
 		ponerBoton("0",insertar);
-		//ponerBoton(",");
-		//ponerBoton("=");
+		ponerBoton(",",orden);
+		ponerBoton("=",orden);
 		
 		add(milamina2,BorderLayout.CENTER);
 
+		ultimaOperacion="="; // Le damos un valor inicial de = para que funcione la calculdadora incialmente, con el fin de que el primer valor lo grabe en los IF de las lineas inferiores
 	}
 	
 	private void ponerBoton(String rotulo,ActionListener oyente) { // es un metodo que agregara los botones y ademas los pondra a la escucha
@@ -108,7 +111,7 @@ class LaminaCalc extends JPanel{
 			
 			String entrada=e.getActionCommand(); //Nos devuelve el string asociado con la accion, capturamos el texto del boton pulsado
 			
-			if (principio){
+			if (principio){ // si no asignamos un comparativo al if siempre lo compara con true
 				
 				pantalla.setText("");
 				
@@ -118,14 +121,64 @@ class LaminaCalc extends JPanel{
 			pantalla.setText(pantalla.getText()+entrada);
 			
 		} 
-		
 				
+	}
+	
+	private class AccionOrden implements ActionListener{ 
+
+		public void actionPerformed(ActionEvent e) { //Se encargara de las operaciones numericas
+			
+			String operacion=e.getActionCommand(); // Almaceno el valior de texto de la operacion
+			
+			//System.out.println("Has pulsado la operacion: "+operacion); // es un modo de confirmar que se esta grabando en la variable operacion el texto asociado a la operacion matematica
+			
+			calcular(Double.parseDouble(pantalla.getText())); // llamamos a un metodo para ir realizando las operaciones matematicos, al cual le pasaremos el parametro del display, ademas convertimos un String en Double
+
+			ultimaOperacion=operacion;
+			
+			principio=true; //reseteamos el display
+			
+		}
+		
+		public void calcular(double x) {
+			
+			if(ultimaOperacion.equals("+")) { //
+				
+				resultado+=x; //suma el valor pulsado a resultado, en caso que sea una suma
+								
+			}else if(ultimaOperacion.equals("=")) { 
+				
+				resultado=x;
+				
+			}else if(ultimaOperacion.equals("-")) {
+				
+				resultado-=x;
+				
+			}else if(ultimaOperacion.equals("/")) {
+				
+				resultado/=x;
+				
+			}else if(ultimaOperacion.equals("x")) {
+				
+				resultado*=x;
+				
+			}
+			
+			pantalla.setText(""+resultado);  //concatenamos la variable resultado que es u numero con un valor vacio para convertirlo en String
+			
+		}
+		
+		
 	}
 	
 	private JPanel milamina2;
 	
 	private JButton pantalla;
 	
-	private boolean principio;
+	private boolean principio; // si no la definimos una variable boolean siempre es false
+	
+	private double  resultado; //variable para grabra los numeros de las operaciones
+	
+	private String ultimaOperacion;
 }
 
